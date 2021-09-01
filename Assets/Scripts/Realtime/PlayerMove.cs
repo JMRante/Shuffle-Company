4eececc4f8eb3650f8885b29d82645f781696fcd
Joshua Stark <joshuamstark@gmail.com>
2021-09-01 00:00:20 -0700

    Fix snapping backwards bug, adding test physics crate

--- Assets/Scripts/Realtime/PlayerMove.cs
+++ Assets/Scripts/Realtime/PlayerMove.cs
@@ -7,57 +7,73 @@ public class PlayerMove : MonoBehaviour
     public float walkSpeed = 3f;
     
     private Vector3 velocity;
+    private Vector3 direction;
     private Vector3 closestSnapPoint;
-    private bool hasInput;
 
     void Start()
     {
         velocity = Vector3.zero;
+        direction = Vector3.zero;
         closestSnapPoint = transform.position;
-        hasInput = false;
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        bool inputUp = Input.GetKey(KeyCode.UpArrow);
+        bool inputRight = Input.GetKey(KeyCode.RightArrow);
+        bool inputDown = Input.GetKey(KeyCode.DownArrow);
+        bool inputLeft = Input.GetKey(KeyCode.LeftArrow);
+
+        if (inputUp)
         {
+            direction = Vector3.forward;
+
             if (Utility.IsSnapped(transform.position))
             {
-                hasInput = true;
                 velocity = Vector3.forward * walkSpeed;
             }
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (inputRight)
         {
+            direction = Vector3.right;
+
             if (Utility.IsSnapped(transform.position))
             {
-                hasInput = true;
                 velocity = Vector3.right * walkSpeed;
             }
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (inputDown)
         {
+            direction = Vector3.back;
+
             if (Utility.IsSnapped(transform.position))
             {
-                hasInput = true;
                 velocity = Vector3.back * walkSpeed;
             }
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (inputLeft)
         {
+            direction = Vector3.left;
+
             if (Utility.IsSnapped(transform.position))
             {
-                hasInput = true;
                 velocity = Vector3.left * walkSpeed;
             }
         }
-        else
+
+        if (!inputUp || !inputRight || !inputDown || !inputLeft)
         {
-            if (hasInput)
+            if (direction != Vector3.zero)
             {
-                closestSnapPoint = Utility.Round(transform.position);
-                hasInput = false;
-                velocity = Vector3.Normalize(closestSnapPoint - transform.position) * walkSpeed;
+                Vector3 tempClosestSnapPoint = Utility.Round(transform.position);
+                Vector3 moveToSnapPointVelocity = Vector3.Normalize(tempClosestSnapPoint - transform.position) * walkSpeed;
+
+                if (moveToSnapPointVelocity.normalized == velocity.normalized)
+                {
+                    direction = Vector3.zero;
+                    velocity = moveToSnapPointVelocity;
+                    closestSnapPoint = tempClosestSnapPoint;
+                }
             }
         }
     }
@@ -66,7 +82,7 @@ public class PlayerMove : MonoBehaviour
     {
         Rigidbody rb = GetComponent<Rigidbody>();
 
-        if (hasInput)
+        if (direction != Vector3.zero)
         {
             rb.MovePosition(transform.position + velocity * Time.deltaTime);
         }
@@ -75,7 +91,7 @@ public class PlayerMove : MonoBehaviour
             Vector3 currentNorm = Vector3.Normalize(closestSnapPoint - transform.position);
             Vector3 overshotNorm = Vector3.Normalize(closestSnapPoint - (transform.position + (velocity * Time.deltaTime)));
 
-            if (!Vector3.Equals(currentNorm, overshotNorm))
+            if (currentNorm != overshotNorm)
             {
                 rb.MovePosition(closestSnapPoint);
                 velocity = Vector3.zero;
